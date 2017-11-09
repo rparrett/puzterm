6df212d4fd776c44f0d767e1a0c3ee3a0ff69de0
rparrett <robparrett@gmail.com>
2017-11-09 19:08:30 +0000

    Handle files with arbitrary data preceding the normal puz header

--- src/puzfile.rs
+++ src/puzfile.rs
@@ -7,6 +7,7 @@ use encoding::{Encoding, DecoderTrap};
 
 #[derive(Debug)]
 pub struct PuzFile {
+    pub preamble: Vec<u8>,
     pub checksum: u16,
     pub magic: String,
     pub cib_checksum: u16,
@@ -40,9 +41,12 @@ named!(null_string_ascii<&[u8], String>,
    )
 );
 
+named!(checksum, terminated!(take!(2), peek!(tag!("ACROSS&DOWN"))));
+
 named!(pub parse_all<&[u8], PuzFile>,
     do_parse!(
-        checksum: le_u16 >>
+        preamble: opt!(many_till!(take!(1), peek!(checksum))) >>
+        checksum: flat_map!(checksum, le_u16) >>
         magic: null_string_ascii >>
         cib_checksum: le_u16 >>
         masked_low_checksum_1: le_u16 >>
@@ -65,7 +69,11 @@ named!(pub parse_all<&[u8], PuzFile>,
         copyright: null_string_ascii >>
         clues: many_m_n!(num_clues as usize, num_clues as usize, null_string_ascii) >>
         notes: null_string_ascii >>
-        ( PuzFile {
+        (PuzFile {
+            preamble: match preamble {
+                Some(p) => p.0.iter().map(|x| x[0]).collect(),
+                _ => Vec::new()
+            },
             checksum: checksum,
             magic: magic,
             cib_checksum: cib_checksum,
